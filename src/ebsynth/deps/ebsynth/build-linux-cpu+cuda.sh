c0de6a66e529d91cebece461afb557e52accefe2
Julian Harris <julian.harris@gmail.com>
2024-03-24 01:42:43 +0000

    Update build-linux-cpu+cuda.sh (#27)
    Ensure the resulting binary is executable

--- src/ebsynth/deps/ebsynth/build-linux-cpu+cuda.sh
+++ src/ebsynth/deps/ebsynth/build-linux-cpu+cuda.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 nvcc -arch compute_50 src/ebsynth.cpp src/ebsynth_cpu.cpp src/ebsynth_cuda.cu -I"include" -DNDEBUG -D__CORRECT_ISO_CPP11_MATH_H_PROTO -O6 -std=c++11 -w -Xcompiler -fopenmp -o bin/ebsynth
+chmod +x bin/ebsynth
